4282a20b5e74281ec3bb3b7aae1e67aa2393e495
Artem Deviatov <dayddhost@yandex.ru>
2026-07-04 17:07:26 +0500

    Step 4: predict_from_scores

--- model.py
+++ model.py
@@ -40,8 +40,13 @@ def compute_scores(x, params):
     scores = x @ w + b
     return scores
 
-# Step 4 - predict_from_scores (not yet solved)
-# TODO: implement
+# Step 4 - predict_from_scores
+import numpy as np
+
+def predict_from_scores(scores):
+    # TODO: convert a 1-D array of raw scores into +1 / -1 class predictions.
+    class_pred = np.where(scores >= 0, 1, -1)
+    return class_pred
 
 # Step 5 - hinge_loss_example (not yet solved)
 # TODO: implement
