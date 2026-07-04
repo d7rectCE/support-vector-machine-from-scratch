ee73159fc7b6075866506203b1679f73edbb0677
Artem Deviatov <dayddhost@yandex.ru>
2026-07-04 17:40:03 +0500

    Step 7: compute_gradients

--- model.py
+++ model.py
@@ -63,8 +63,20 @@ def svm_objective(x, y, params, reg_lambda):
     obj = 1/len(x) * np.sum(loss + reg_lambda * (w @ w))
     return obj
 
-# Step 7 - compute_gradients (not yet solved)
-# TODO: implement
+# Step 7 - compute_gradients
+import numpy as np
+
+def compute_gradients(x, y, params, reg_lambda):
+    """Return {'dw': ndarray shape (n_features,), 'db': float} = gradient of svm_objective."""
+    n_samples = x.shape[0]
+    w = params["w"]
+    b = params["b"]
+    margins = y * (x @ w + b)
+    violation_mask = (margins < 1)
+    
+    dw = -(1/n_samples) * (x.T @ (y * violation_mask)) + 2 * reg_lambda * w
+    db = -(1/n_samples) * np.sum(y * violation_mask)
+    return {"dw": dw, "db": db}
 
 # Step 8 - apply_update (not yet solved)
 # TODO: implement
