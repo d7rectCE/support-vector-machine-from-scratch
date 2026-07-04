2fd1229315b7f4cf3f88d0105e6ecc59bbd18620
Artem Deviatov <dayddhost@yandex.ru>
2026-07-04 17:54:09 +0500

    Step 10: predict_labels

--- model.py
+++ model.py
@@ -92,8 +92,12 @@ def train_svm(x, y, learning_rate, reg_lambda, n_epochs):
         params = apply_update(params, grads, learning_rate)
     return params
 
-# Step 10 - predict_labels (not yet solved)
-# TODO: implement
+# Step 10 - predict_labels
+def predict_labels(x, params):
+    # TODO: return an array of {-1, +1} labels, one per row of x, using params['w'] and params['b'].
+    scores = compute_scores(x, params)
+    y_pred = predict_from_scores(scores)
+    return y_pred
 
 # Step 11 - accuracy_score (not yet solved)
 # TODO: implement
